c4680dc2d67701d17ca60aba807199331108ffd3
Jerimiah Huston <afknscientist@gmail.com>
2019-04-11 19:07:21 -0400

    adding build for deployment

---
+++ build/precache-manifest.164ea3ce4f4975eb67f8b3f63fc8acc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03922a2284e2590f7a42",
+    "url": "/static/css/main.9c2b06c1.chunk.css"
+  },
+  {
+    "revision": "03922a2284e2590f7a42",
+    "url": "/static/js/main.890d45f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "49371899a3409ed76a92",
+    "url": "/static/css/2.af2b33c1.chunk.css"
+  },
+  {
+    "revision": "49371899a3409ed76a92",
+    "url": "/static/js/2.a33dbe55.chunk.js"
+  },
+  {
+    "revision": "e04b4ab2ee98e1d3a0888ab6b83a3377",
+    "url": "/index.html"
+  }
+];
